23117e1c7a33115629d1e6e37c8394449a4bef87
poddamatt98 <podda.matt@gmail.com>
2024-08-09 10:51:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import loadConfig from './components/ConfigLoader';
 import WelcomePage from './components/WelcomePage';
 import CompetitionName from './components/CompetitionName';
